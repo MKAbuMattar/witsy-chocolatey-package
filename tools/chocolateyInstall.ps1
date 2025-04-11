e2e9966e18456a42e63ba83494ee35fc71602d42
MKAbuMattar <mohammad.khaled@outlook.com>
2025-04-11 23:20:27 +0300

    chore: downgrade version to 1.41.0 and update installation script with new download URL and checksum

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop';
 $packageName = 'Witsy'
-$url = 'https://github.com/nbonamy/witsy/releases/download/v1.41.1/Witsy-1.41.1-win32-x64.Setup.exe'
+$url = 'https://github.com/nbonamy/witsy/releases/download/v1.41.0/Witsy-1.41.0-win32-x64.Setup.exe'
 $installerType = 'exe'
-$checksum = '45F5FE3D0AA603C3398C1F1EEC0804EB716086BCB1438CF71439222D151C75F3'
+$checksum = 'EE6C2A305E8C74D5D9534B05B371C93F33DDB3393AC30DCA256DDB4D1141098A'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0)
