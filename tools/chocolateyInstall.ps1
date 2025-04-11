6779121c982028e048ce7afa7197a4ec257b9f6a
MKAbuMattar <mohammad.khaled@outlook.com>
2025-04-11 22:45:25 +0300

    chore: bump version to 1.37.3 and update installation script for new download URL and checksum

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop';
 $packageName = 'Witsy'
-$url = 'https://github.com/nbonamy/witsy/releases/download/v1.37.2/Witsy-1.37.2-win32-x64.Setup.exe'
+$url = 'https://github.com/nbonamy/witsy/releases/download/v1.37.3/Witsy-1.37.3-win32-x64.Setup.exe'
 $installerType = 'exe'
-$checksum = '7bf54ec9139f1fb3f08b6d0d6a8872bfbe757a717061f3a199ed23b6b80d513e'
+$checksum = '1C65E598E6F85B0CEFDFB94966BC0909C2C0AC5FDEDD012616C4E94BBE6E0BEE'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0)
