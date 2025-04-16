f07cf73d03b07061eb727ee9c48f9f3ab54e2e99
MKAbuMattar <mohammad.khaled@outlook.com>
2025-04-16 19:25:46 +0300

    chore: bump version to 2.4.4 and update installer URL and checksum

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop';
 $packageName = 'Witsy'
-$url = 'https://github.com/nbonamy/witsy/releases/download/v2.4.3/Witsy-2.4.3-win32-x64.Setup.exe'
+$url = 'https://github.com/nbonamy/witsy/releases/download/v2.4.4/Witsy-2.4.4-win32-x64.Setup.exe'
 $installerType = 'exe'
-$checksum = 'BE7F9D2277752D3C57E39C2A10704E186C0FAE67B413306ADE003C877E64D3D8'
+$checksum = '607F0BB5947FAA6F6751416D6A493DCCBBC691F53F3F1F8516F573F8DE04B88C'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0)
