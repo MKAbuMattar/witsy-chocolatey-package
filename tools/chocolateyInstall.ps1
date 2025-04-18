2e53810bba22d463723e3ac751224f9f39c72413
MKAbuMattar <mohammad.khaled@outlook.com>
2025-04-18 07:38:53 +0300

    chore: update version to 2.4.8 and modify installer URL and checksum

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop';
 $packageName = 'Witsy'
-$url = 'https://github.com/nbonamy/witsy/releases/download/v2.4.7/Witsy-2.4.7-win32-x64.Setup.exe'
+$url = 'https://github.com/nbonamy/witsy/releases/download/v2.4.8/Witsy-2.4.8-win32-x64.Setup.exe'
 $installerType = 'exe'
-$checksum = '17C86EB6DADE70EC2F5E4D7CAE2910B5639B968ABD22B5C21C5F427B497E4D98'
+$checksum = '03081BEFD5AC10A5466342ED67E1CBBCA8352784F2E12FF7777D11DEB12748CE'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0)
