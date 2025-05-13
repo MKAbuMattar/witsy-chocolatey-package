70c0fc960e255f045b0e28db0e71fa350d92c68e
MKAbuMattar <mohammad.khaled@outlook.com>
2025-05-14 01:13:51 +0300

    fix: update silent installation arguments for Witsy installer

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $url = 'https://github.com/nbonamy/witsy/releases/download/v2.5.1/Witsy-2.5.1-wi
 $installerType = 'exe'
 $checksum = '235FFBB0924F365CE2F87B6DC06538A79D488C9BA45B251D5F6AA436D9849286'
 $checksumType = 'sha256'
-$silentArgs = '/S'
+$silentArgs = '/S /quiet'
 $validExitCodes = @(0)
 
 $installPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
