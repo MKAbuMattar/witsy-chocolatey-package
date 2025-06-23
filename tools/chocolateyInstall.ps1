2c0d54d10a760bf8216a4a10d4c24ae4b089707f
Mohammad Abu Mattar <mohammad.khaled@outlook.com>
2025-06-23 04:16:23 +0300

    Update Witsy to v2.9.0 (#67)

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop';
 $packageName = 'Witsy'
-$url = 'https://github.com/nbonamy/witsy/releases/download/v2.8.2/Witsy-2.8.2-win32-x64.Setup.exe'
+$url = 'https://github.com/nbonamy/witsy/releases/download/v2.9.0/Witsy-2.9.0-win32-x64.Setup.exe'
 $installerType = 'exe'
-$checksum = '135962EDC9E261CDCF261FD7B7354ED658F8D579DEB63D3862D6159696D115B1'
+$checksum = '32321DB9A3D1CFB35796791B3DE72A76B838635BE5B2F1ADEEA8DF0C8699AF80'
 $checksumType = 'sha256'
 $silentArgs = '/S /quiet'
 $validExitCodes = @(0)
