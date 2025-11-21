4e2d743f9ff9949c358efe704f4bb1ed40f3cf09
Mohammad Abu Mattar <mohammad.khaled@outlook.com>
2025-11-21 18:35:50 +0300

    chore: update Witsy to version 3.2.0 and adjust installation script (#99)
    * fix: update silent installation arguments for Witsy installer
    
    * chore: update Witsy to version 3.2.0 and adjust installation script

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop';
 $packageName = 'Witsy'
-$url = 'https://github.com/nbonamy/witsy/releases/download/v3.1.1/Witsy-3.1.1-win32-x64.Setup.exe'
+$url = 'https://github.com/nbonamy/witsy/releases/download/v3.2.0/Witsy-3.2.0-win32-x64.Setup.exe'
 $installerType = 'exe'
-$checksum = '548EC1AEDE6F8B7DFDDFE9958A4AAA26A5F496D7B1F20D73B088F8D01051A1EC'
+$checksum = '12E314DB4CDD40D5474C884551D571A8607CE1828A8D10C1F062D594B68E9106'
 $checksumType = 'sha256'
 $silentArgs = '/S /quiet'
 $validExitCodes = @(0)
@@ -11,12 +11,12 @@ $installPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $downloadFile = Join-Path $installPath "$($packageName)Setup.exe"
 
 Get-ChocolateyWebFile -PackageName $packageName `
-    -FileFullPath $downloadFile `
-    -Url $url `
-    -Checksum $checksum `
-    -ChecksumType $checksumType
+  -FileFullPath $downloadFile `
+  -Url $url `
+  -Checksum $checksum `
+  -ChecksumType $checksumType
 Install-ChocolateyInstallPackage -PackageName $packageName `
-    -FileType $installerType `
-    -SilentArgs $silentArgs `
-    -File $downloadFile `
-    -ValidExitCodes $validExitCodes
+  -FileType $installerType `
+  -SilentArgs $silentArgs `
+  -File $downloadFile `
+  -ValidExitCodes $validExitCodes
