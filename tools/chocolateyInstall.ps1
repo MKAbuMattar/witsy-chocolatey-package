f77ea4746419f78564a44e8b1d6e27b64abcef6a
MKAbuMattar <mohammad.khaled@outlook.com>
2026-01-30 01:27:54 +0000

    Update Witsy to v3.5.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop';
 $packageName = 'Witsy'
-$url = 'https://github.com/nbonamy/witsy/releases/download/v3.4.2/Witsy-3.4.2-win32-x64.Setup.exe'
+$url = 'https://github.com/nbonamy/witsy/releases/download/v3.5.0/Witsy-3.5.0-win32-x64.Setup.exe'
 $installerType = 'exe'
-$checksum = 'EBD1615F6D478D270AEC172601684DA25F3F357CD6122F3AC7B6605D2E99B784'
+$checksum = 'FFF207EF44F67CC5031B0DDD42F96197FD6E94C24592BE228EACE0891EDF86C1'
 $checksumType = 'sha256'
 $silentArgs = '/S /quiet'
 $validExitCodes = @(0)
